4418fb61c1a2ceda7fd6879c01cc0231dec99cd9
hmdmrndng <hmdmrndng@gmail.com>
2026-02-22 22:44:23 +0800

    next onfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb', // Increase this to 5MB, 10MB, or whatever you prefer!
+    },
+  },
   /* config options here */
   reactCompiler: true,
 };
